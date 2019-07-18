49a477da594aae0c93925a9cebc3c22b66ccd8f1
dominisz <dominisz@gmail.com>
2019-07-18 21:06:46 +0200

    Create ShapeFactory

--- src/main/java/pl/dominisz/App.java
+++ src/main/java/pl/dominisz/App.java
@@ -1,13 +1,15 @@
 package pl.dominisz;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+  public static void main(String[] args) {
+    ShapeFactory shapeFactory = new ShapeFactory();
+
+    Shape circle = shapeFactory.getShape("circle");
+    Shape square = shapeFactory.getShape("square");
+    Shape rectangle = shapeFactory.getShape("rectangle");
+
+    circle.draw();
+    square.draw();
+    rectangle.draw();
+  }
 }
